b40d697c83c3d281fac67f5e8b92a304808cde99
eomziyi <2119545103@qq.com>
2026-03-23 00:12:53 +0800

    Delete backend/auth/config.py

--- backend/auth/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = 'your-secret-key-here'
-    # 使用 SQLAlchemy 的标准格式
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///users.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
